1f3e01170a27c77670186b2b89040877622a5d6c
Retr01234 <>
2023-08-13 12:03:33 +0000

    featuer: rendering data

--- crud/views.py
+++ crud/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 # Defining a Greeting Method to send and receive a HTTP Request
 
 
 def get_todo_items(request):
-    return render(request, 'crud/crud_app.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'crud/crud_app.html', context)
